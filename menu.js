1d0e6a09b2f037d5b635ea5db9371aa0c634b947
ppmp556 <n.adamuk65@gmail.com>
2020-07-06 22:20:30 +0900

    ソースツリーテスト

---
+++ menu.js
@@ -0,0 +1,18 @@
+const toggler = document.getElementById('navToggle');
+//#navToggleを取得し、togglerに格納
+
+
+const navList = document.getElementById('navLists');
+//#navListsを取得し、navListに格納
+
+
+toggler.addEventListener('click', function() {　
+    //togglerクリックされたら、
+    
+    
+    toggler.classList.toggle('clicked');　
+    //clickedクラスがあれば削除
+    //clickedクラスがなければ付与
+    
+    navList.classList.toggle('clicked');
+});
